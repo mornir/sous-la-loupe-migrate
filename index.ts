de07ceee1f42ac3a07ed2e61a0e94f578bd229ae
mornir <jeromepott@hotmail.com>
2023-12-23 20:12:14 +0100

    wip: write function

--- index.ts
+++ index.ts
@@ -1,16 +1,32 @@
 const path = "./data/german.json";
+const path2 = "./data/french.json";
 const file = Bun.file(path);
+const file2 = Bun.file(path2);
 
 const vedettes = await file.json();
 
-const data = vedettes.map((v) => {
-  if(!v.term) return null
-  return {
-    id: v.id_term,
-    term: v.term.trim()
-  }
-}).filter(Boolean)
+const translations = await file2.json();
 
-const output = JSON.stringify(data)
+const data = vedettes
+  .map((v) => {
+    if (!v.term) return null;
+
+    const traductions = translations.filter((t) => v.id_term === t.id_term).map(t2 => {
+      return {
+        id: t2.id_translation,
+        terme: t2.term_translation.trim(),
+        groupe: t2.display_order
+      }
+    });
+
+    return {
+      id: v.id_term,
+      term: v.term.trim(),
+      traductions
+    };
+  })
+  .filter(Boolean);
+
+const output = JSON.stringify(data);
 
 await Bun.write("fichier.json", output);
